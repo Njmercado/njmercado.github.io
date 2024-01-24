fb5884f869ad82f29100e73e1f5c689ebf660d30
Nino Mercado <njmercado@uninorte.edu.co>
2024-01-24 17:54:39 -0500

    feat: created index for container components

---
+++ src/components/Container/index.ts
@@ -0,0 +1,11 @@
+import { type IContainer } from "./dto/IContainer";
+import ContainerDTO from "./dto/Container.dto";
+import Container from "./Container";
+import NoteContainer from "./NoteContainer";
+
+export {
+  IContainer,
+  ContainerDTO,
+  Container,
+  NoteContainer
+}
