576fe7080a06e1feac48fc5471d4cc21107c9a3e
Nino Mercado <njmercado@uninorte.edu.co>
2024-01-25 14:32:17 -0500

    feat: added InfoContainer to container index

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -2,10 +2,12 @@ import { type IContainer } from "./dto/IContainer";
 import ContainerDTO from "./dto/Container.dto";
 import Container from "./Container";
 import NoteContainer from "./NoteContainer";
+import InfoContainer from "./InfoContainer";
 
 export {
   IContainer,
   ContainerDTO,
   Container,
-  NoteContainer
+  NoteContainer,
+  InfoContainer,
 }
