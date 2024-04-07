63caa136fde4582d149dbb3e47f0941d3df15390
Nino Mercado <njmercado@uninorte.edu.co>
2024-04-07 15:02:03 -0500

    feat: semi template of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,28 @@ export default function Home() {
         // TODO: move this to a function
         <div className="grid grid-cols-6  w-full h-fit my-2">
           <div className="col-span-2 flex justify-center items-center">
-            <img className='rounded-full border-black border-2' style={{height: '50px', width: '50px'}} src={ITEMS[0]} alt="" />
+            <img className='rounded-full border-black border-2' style={{ height: '50px', width: '50px' }} src={ITEMS[0]} alt="" />
           </div>
           <div className="col-span-4 flex flex-col w-full">
             <div className="p-2">Project Name</div>
             <div className="grid grid-cols-2">
-              <div className="bg-gray-400 text-white col-span-1 mx-2 text-center" style={{borderRadius: '10px'}}>tech</div>
+              <div className="bg-gray-400 text-white col-span-1 mx-2 text-center" style={{ borderRadius: '10px' }}>tech</div>
               <div className="bg-gray-400 text-white col-span-1 mx-2 rounded-xl text-center">tech 2</div>
             </div>
           </div>
+        </div>
+      )
+    }
+
+    return projects;
+  }
+
+  const getProjectsCards = () => {
+    let projects: any[] = []
+    for (var i = 0; i < 7; i++) {
+      projects.push(
+        <div>
+          
         </div>
       )
     }
@@ -47,15 +60,29 @@ export default function Home() {
             <div>Contact</div>
           </div>
           <div className="w-full h-1/2 relative">
-            <div className='absolute max-h-full w-full overflow-scroll scroll-smooth overflow-x-hidden' style={{scrollbarWidth: 'none'}}>
+            <div className='absolute max-h-full w-full overflow-scroll scroll-smooth overflow-x-hidden' style={{ scrollbarWidth: 'none' }}>
               {
                 getProjects().map(project => project)
               }
             </div>
           </div>
         </div>
+        {/* This zone is to show information */}
         <div className="col-span-8 h-full bg-gray-100">
+          <div className=" flex flex-col items-center w-full" style={{ padding: '5% 20%' }}>
+            <div>HOME</div>
+            <div>-------------------------------------------------------------------------------------------------------------------</div>
+            <div>
+              Hi, Nino here. In simple words I am an Artist by hobbie and Designer by heart and a Developer by passion. 
+              I like simple and functional things without the necessity of weird solutions. My sign is 
+              "perfection do not exists but there is always a better solution".
+              <span className='bg-gray-500 text-white ml-2 rounded-sm p-0.5'>Lets go and know more about me {":)"}</span>
+            </div>
+            <div>-------------------------------------------------------------------------------------------------------------------</div>
+            <div>
 
+            </div>
+          </div>
         </div>
       </div>
       {/* this part gonna control the pages */}
