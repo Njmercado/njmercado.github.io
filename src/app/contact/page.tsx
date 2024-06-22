fc4ea400db37bd312c857db6e82b82152a23f3e4
Nino Mercado <njmercado@uninorte.edu.co>
2024-06-22 17:09:49 -0500

    feat: contact information
    * Added code to contact page
    * Created constants to store profile links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { CV_LINKS } from "@/constants/general.constant";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
 export default function Contact() {
+
+  const goTo = (link: string) => { window.open(link, '_blank') }
+  const goToLinkedIn = () => goTo(CV_LINKS.LINKEDIN)
+  const goToGithub = () => goTo(CV_LINKS.GITHUB)
+
   return (
-    <div>hola desde contact</div>
+    <article className="flex flex-col justify-center items-center">
+      <section className="flex flex-row items-center">
+        <Image src="/static/atlantico.png" width={300} height={300} alt="Image of Colombian Atlantic's department."/>
+        <p> <em>Barranquilla - Colombia</em> </p>
+      </section>
+      <section className="mt-10">
+        <p className="flex gap-5">
+          <span className="cursor-pointer"><FontAwesomeIcon icon={faLinkedin} size={'2xl'} onClick={goToLinkedIn}/></span>
+          <span className="cursor-pointer"><FontAwesomeIcon icon={faGithub} size={'2xl'} onClick={goToGithub}/></span>
+        </p>
+      </section>
+    </article>
   )
 }
--- src/constants/general.constant.ts
+++ src/constants/general.constant.ts
@@ -2,3 +2,7 @@ import { faAngular, faDigitalOcean, faGithub, faNodeJs, faPython, faReact, faVue
 
 export const TECHS_I_KNOW = [faReact, faVuejs, faAngular, faNodeJs, faPython, faGithub, faDigitalOcean]
 export const CV_PDF = '/cv_nino_mercado.pdf'
+export const CV_LINKS = {
+  LINKEDIN: 'https://www.linkedin.com/in/nino-mercado-48b513147/',
+  GITHUB: 'https://github.com/Njmercado'
+}
