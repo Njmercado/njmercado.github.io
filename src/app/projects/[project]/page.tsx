ffd120744c4380ca249faa8ba3202a19b207b0fa
Nino Mercado <njmercado@uninorte.edu.co>
2024-06-27 03:31:18 -0500

    fix(deploy): deleted 'use client'

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useParams, useRouter } from "next/navigation"
 import { EMPTY_PROJECT_TEMPLATE, PROJECTS } from "@/constants/projects.constant";
 import { Title, TitleSize } from "@/components";
